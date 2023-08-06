34473f25f2372e56447953adb7ab5073d79904af
Ali zokaei <47479098+AliZokaeai@users.noreply.github.com>
2023-08-07 00:07:28 +0330

    work deep 01 2023-08-07 start Python

--- 004 Python/Lesson 001/les.py
+++ 004 Python/Lesson 001/les.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print ("Hi ali you can do it! \n you are a data scientist now!")
+print ("1")
+print (-10)
+print (-10.5)
+print (True)
+print((0.1+0.1+0.1)/3)
+print(format(0.1,'.25f'))
+print(format(0.3,'.25f'))
+print (((0.1+0.1+0.1)-0.3)< 0.1)
+print (1+1+1 == 3)
+
+
