fb2abf846e8991b8d45fd77c8a090d168e9f4f2e
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-02-28 22:32:42 +0330

    learn more about JavaScript

---
+++ 003 JavaScripts/excersice01.js
@@ -0,0 +1,10 @@
+// this is comment
+/* this is multiline comment
+*/
+
+console.log("\u{1f600}")
+
+console.log(1_000_000_000);
+console.log(0x89_ab_cd_ef);
+console.log(0b0001_1101_0111);
+console.log(0.123_456_789);
