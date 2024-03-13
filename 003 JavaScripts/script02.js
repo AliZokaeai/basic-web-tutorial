ba136617e655950710e0044eb8d35a35bcd15d14
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-03-13 22:41:15 +0330

    learn more about JavaScript

--- 003 JavaScripts/script02.js
+++ 003 JavaScripts/script02.js
@@ -1,7 +1,15 @@
-const temp = 20;
+let loggedIn = false;
+let username;
+let password;
 
-if(temp >0 && temp <=30){
-    console.log("The weather is Good");
-}else{
-    console.log("The weather is Bad");
-}
+do{
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username ==="myUserName" && password ==="myPassword"){
+        loggedIn = ture;
+        console.log("You are logged in!");
+    }else{
+        console.log("Invalid credentials! Plaease try again");
+    }
+}while(!loggedIn)
