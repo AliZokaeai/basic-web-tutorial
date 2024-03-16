c5315ac7257a6a1b24da688e967bdb86c3b55e4d
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-03-16 23:28:43 +0330

    learn more about JavaScript

--- 003 JavaScripts/ex001/script.js
+++ 003 JavaScripts/ex001/script.js
@@ -1,1 +1,17 @@
-console.log([-2, [-1, 3, [4]], 1, 2].flatMap((x) => (x < 0 ? [] : Math.sqrt(x))));
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+function convert() {
+  if (toFahrenheit.checked) {
+    temp = Math.round((Number(textBox.value) * 9) / 5 + 32);
+    result.textContent = `Temp is ${temp} °F`;
+  } else if (toCelsius.checked) {
+    temp = Math.round((Number(textBox.value) - 32) * (5 / 9));
+    result.textContent = `Temp is ${temp} °C`;
+  } else {
+    result.textContent = "Select a unit!";
+  }
+}
