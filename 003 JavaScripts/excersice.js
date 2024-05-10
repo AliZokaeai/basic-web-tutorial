fd93058d913b29de90a57329ef5ed9b006e64d92
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-05-10 22:49:12 +0330

    JavaScript classes

--- 003 JavaScripts/excersice.js
+++ 003 JavaScripts/excersice.js
@@ -1,6 +1,18 @@
-let a = { 0: "a", 1: "b", 2: "c", length: 3 };
+function printprops(o) {
+  for (let p in o) {
+    console.log(`${p}: ${o[p]}\n`);
+  }
+}
 
-console.log(Array.prototype.join.call(a, "+"));
-console.log(Array.prototype.map.call(a, (x) => x.toUpperCase()));
-console.log(Array.prototype.slice.call(a, 0));
-console.log(Array.from(a));
+function distance(y1, y2, x1, x2) {
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function factorial(x){
+  if(x<=1)return 1;
+  return x *factorial(x-1);
+}
+
+pri
