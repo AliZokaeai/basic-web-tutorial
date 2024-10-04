59f094a47183dbee0305d1a41693b9a13d0d0f41
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-10-04 12:59:17 +0330

    Doing more website madules

---
+++ 001 HTML Component/HTML-Lesson-024-sidenav-without-animation/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("main").style.marginLeft = "";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("main").style.marginLeft = "0";
+}
