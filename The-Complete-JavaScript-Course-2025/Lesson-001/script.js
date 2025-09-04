b9f6280f356838db2a66c05f346ba40820f4531a
AliZokaeai <alizokaeaidehaghani@gmail.com>
2025-09-04 13:39:51 +0330

    add more components to projects

--- The-Complete-JavaScript-Course-2025/Lesson-001/script.js
+++ The-Complete-JavaScript-Course-2025/Lesson-001/script.js
@@ -1,15 +1,2 @@
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarag = now - 2018;
-
-console.log(ageJonas, ageSarag);
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-
-const fistName = "Jonas";
-const lastName = "Schmedant";
-console.log(fistName + " " + lastName);
-
-let x = 10 + 5;
-x *= 10;
-console.log(x);
+const age = "18";
+if (age === 18) console.log;
