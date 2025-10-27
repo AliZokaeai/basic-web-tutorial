0cf5cf554911fb1b24aab62158ae931041f6f806
AliZokaeai <alizokaeaidehaghani@gmail.com>
2025-10-27 21:28:30 +0330

    add more codes to my project

--- The-Complete-JavaScript-Course-2025/Lesson-011/script.js
+++ The-Complete-JavaScript-Course-2025/Lesson-011/script.js
@@ -81,20 +81,20 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       console.log(arr);
       return int >= 1;
@@ -114,7 +114,39 @@ const createUsernams = function (accs) {
 };
 console.log(createUsernams(accounts));
 
-console.log(accounts);
+console.log('accounts:', accounts);
+//Event handler
+
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  //Prevent form from submiting
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log('Current Account:', currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log('LOGED IN');
+    //display ui and message
+    labelWelcome.textContent = `Welcom back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    // display movements
+    displayMovements(currentAccount.movements);
+
+    //diplay balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
@@ -237,3 +269,11 @@ const totalDepositsUSD = movements
   })
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log('Movements:', movements);
+console.log('First With Drawal:', firstWithdrawal);
+console.log('accounts:', accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log('account:', account);
